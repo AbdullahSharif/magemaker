78320eb951f00ddab520f56e28bdcb4ee31aa837
AbdullahSharif <abdullah.29.umar@gmail.com>
2025-05-08 17:05:48 +0500

    test

--- magemaker/runner.py
+++ magemaker/runner.py
@@ -162,14 +162,6 @@ if __name__ == '__main__':
     )
 
     parser.add_argument(
-        '--query',
-        action='query',
-        help="path to YAML query configuration file",
-        type=str
-    )
-
-
-    parser.add_argument(
         "--hf",
         help="Deploy a Hugging Face Model.",
         type=str
