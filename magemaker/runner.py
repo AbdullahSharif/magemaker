9623e083857fd856cccd0d9922291b3ca612a525
AbdullahSharif <abdullah.29.umar@gmail.com>
2025-05-16 21:50:20 +0500

    test 6

--- magemaker/runner.py
+++ magemaker/runner.py
@@ -179,6 +179,13 @@ if __name__ == '__main__':
         type=str
     )
 
+    parser.add_argument(
+        '--query-toml',
+        action='query',
+        help="path to TOML query configuration file",
+        type=str
+    )
+
     
     parser.add_argument(
         "--train",
